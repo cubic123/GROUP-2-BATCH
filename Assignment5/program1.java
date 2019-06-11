9b991984ffdb603d1704255b8d98600cb71128c6
ranagroup <51347298+ranagroup@users.noreply.github.com>
2019-06-11 17:39:29 -0400

    added by rana

---
+++ Assignment5/program1.java
@@ -0,0 +1,21 @@
+
+
+public class program1 {
+
+	public static void main(String[] args) {
+		System.out.println("-------------------------") ;
+		System.out.println("                         ") ;
+		System.out.println("|    @             @    |") ;
+		System.out.println("                         ") ;
+		System.out.println("|          |            |") ;
+		System.out.println("                         ") ;
+		System.out.println("|                       |") ;
+		System.out.println("-------------------------") ;
+		System.out.println("          /|\\          ") ;
+		System.out.println("          /|\\          ") ;
+		System.out.println("          /|\\          ") ;
+		
+
+	}
+
+}
