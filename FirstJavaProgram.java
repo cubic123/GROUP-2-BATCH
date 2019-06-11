9ceec720043353b5748f951cce72b3a08c47c264
Subash cubic <subash@cubicit.net>
2019-06-10 21:12:46 -0400

    added more hw

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -2,8 +2,10 @@ class FirstJavaProgram{
 
 public static void main(String args []){
 
-
-System.out.println("THis is our first class");
+int number = 2;
+int number1 = 5 ;
+int result = number + number1;
+System.out.println("The result of number and number1 is := " + result);
 }
 
 }
