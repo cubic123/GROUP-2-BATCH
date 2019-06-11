37b35bdbe57c27987e0aa22fd935361748bc6c1a
ranagroup <51347298+ranagroup@users.noreply.github.com>
2019-06-11 17:38:35 -0400

    Add files via upload
    rana

---
+++ program2.java
@@ -0,0 +1,20 @@
+
+
+public class program2 {
+
+
+	public static void main(String[] args) {
+		String name="Mohammed Chowdhury";
+        int age =30;
+		char sex = 'M';
+		float salary=102;
+		
+		System.out.println("Name:"+name);
+		System.out.println("age:"+age);
+		System.out.println("Gender:"+sex);
+		System.out.println("salary:"+salary);
+}
+}
+               
+		
+	
