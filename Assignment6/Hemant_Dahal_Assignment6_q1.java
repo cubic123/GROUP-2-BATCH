537a997522ccd1f66233ae9e624c8f36e432e8a1
hemanta <hemantdahal1234@gmail.com>
2019-06-12 13:46:44 -0400

    this is Assigment6Q1

---
+++ Assignment6/Hemant_Dahal_Assignment6_q1.java
@@ -0,0 +1,24 @@
+class Hemant_Dahal_Assignment6_q1 {
+public static void main(String[] args){
+int x = 36;
+int y = 9;
+int z = 3;
+
+
+
+int a = 5;
+int b = 15;
+
+a += b;
+b -= a;
+System.out.println("Result:" + a);
+System.out.println("Result:" + b);
+System.out.println(x+y-z);
+System.out.println(x-y-z);
+System.out.println(x*y*z);
+System.out.println(x/y/z);
+System.out.println(x%y%z);
+System.out.println(x+y+z);
+}
+}
+
