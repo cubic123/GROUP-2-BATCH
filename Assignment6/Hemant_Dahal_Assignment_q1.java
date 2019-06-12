0a2a93888309fde62ee7893746e1fad06ebd98e2
hemanta <hemantdahal1234@gmail.com>
2019-06-12 12:29:35 -0400

    Assignment 5 add from Hemant

---
+++ Assignment6/Hemant_Dahal_Assignment_q1.java
@@ -0,0 +1,18 @@
+class Hemant_Dahal_Assignment_q1 {
+public static void main(String[] args) {
+int Phone_no =422567345;
+short id_no = 01;
+long Acc_no = 8782;
+double Amount = 8848894.98;
+boolean x = true;
+float amount = 125355.746f;
+
+System.out.println("Id_no:" + id_no);
+System.out.println("Phone_no:" + Phone_no);
+System.out.println("Acc_no:" + Acc_no);
+System.out.println("Amount:" + Amount);
+System.out.println("Absolute:" + x);
+System.out.print("Desimal:" + amount);
+}}
+
+
