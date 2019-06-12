ee8605b8a3e20456fccafb0509a48efb3b237288
Meen Khatri <meenkhatri@Meen-Khatris-MacBook-Pro.local>
2019-06-12 19:48:40 -0400

    Meen Khatri

---
+++ Assignment6/Meen_Khatri_Assignmen6t_q1.java
@@ -0,0 +1,25 @@
+public class Meen_khatri_Assignment6_q1 {
+
+	public static void main(String[] args) {
+		int num1 = 10; 
+		int num2 = 25;
+		int a;
+		int b;
+		int c;
+		a = 21;
+		b = ++a;
+		c = b++ + ++a;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+		
+
+	}
+
+}
+
