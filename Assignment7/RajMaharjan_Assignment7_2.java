8f93ac076d208785baf7695f131acba49af5fc65
raajmaharjan <raaj.maharjan@outlook.com>
2019-06-13 18:29:55 -0400

    my assignment

---
+++ Assignment7/RajMaharjan_Assignment7_2.java
@@ -0,0 +1,17 @@
+package javaclass;
+
+public class RajMaharjan_Assignment7_2 {
+
+	int n =5;{
+	
+	if (n % 2 ==0){
+		System.out.println("the number is even");
+	}
+	else
+	{
+		System.out.println("the number is odd");
+	}
+	}
+	}
+	
+
---
+++ Assignment7/RajMaharjan_Asssignment7_1.java
@@ -0,0 +1,20 @@
+public class RajMaharjan_Assignment7_1 {
+
+	public static void main(String[] args) {
+		
+		int number=3;
+        if (number > 0) {
+                System.out.println(number+ "is a positive number");
+}
+else if (number < 0){
+        System.out.println(number+"is a negative number");
+
+}
+else{
+System.out.println(number+ "is neither positive nor negative");
+
+        }
+
+	}
+
+}
