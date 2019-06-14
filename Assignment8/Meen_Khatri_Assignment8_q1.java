9b59808389aa001b7343cd14a5fb1fccf56aefbc
pkcmeen2003 <51353207+pkcmeen2003@users.noreply.github.com>
2019-06-14 14:39:20 -0400

    Add files via upload

---
+++ Assignment8/Meen_Khatri_Assignment8_q1.java
@@ -0,0 +1,11 @@
+
+
+public class Meen_Khatri_Assignment8_q1 {
+	
+		public static void main(String[] args) {
+			// Creating for loop to repeat my name10 times
+			for(int i  = 0; i< 10; i ++)
+			System.out.println("Meen Khatri" );
+
+}
+}
