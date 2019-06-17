a949b8f1dc59b22cf2542308b4d039b928f5fcb1
ranagroup <51347298+ranagroup@users.noreply.github.com>
2019-06-17 19:32:36 -0400

    Add files via upload

---
+++ Assignment8/MohammedChowdhury_assainmrnt8_q1.java
@@ -0,0 +1,13 @@
+package assainment8;
+
+public class MohammedChowdhury_assainmrnt8_q1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=20;i++) {
+			System.out.println("RANA "+i);
+			
+		}
+
+	}
+
+}
---
+++ Assignment8/MohammedChowdhury_assainmrnt8_q4.java
@@ -0,0 +1,21 @@
+package assainment8;
+
+public class MohammedChowdhury_assainmrnt8_q4 {
+
+	public static void main(String[] args) {
+		
+		for(int row=5;row>=1;row--) {
+			for(int col=1; col<=row;col++) {
+			
+			
+			System.out.println("*"+col);
+			
+			}
+			System.out.println("*"+row);
+		}
+		
+
+	}
+
+}
+	
