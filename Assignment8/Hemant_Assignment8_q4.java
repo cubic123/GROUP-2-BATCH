0ad0fad5adb5352d1c9bebcd99060dd5920cbae4
Subash Shrestha <subash@cubicit.net>
2019-06-18 20:10:29 -0400

    changes

--- Assignment8/Hemant_Assignment8_q4.java
+++ Assignment8/Hemant_Assignment8_q4.java
@@ -1,12 +1,10 @@
-package hemantjava1;
 
-public class Multitable {
+public class Hemant_Assignment8_q4 {
 	public static void main(String[] args) {
 		int h=5;
 		for(int k=1;k<=10;k++) {
 			System.out.println(h + "*" + k + "=" + (h*k));
 		}
 	}
-
 }
 
