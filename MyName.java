59555d8a086735c7ca12683ade229b07a47a17b0
Meen Khatri <meenkhatri@MeenKhatrisMBP.fios-router.home>
2019-06-18 22:50:01 -0400

    Test

---
+++ MyName.java
@@ -0,0 +1,24 @@
+
+public class MyName {
+
+	public static void main(String[] args) {
+		
+		MyNameIs();
+		MyNameIs();
+		MyNameIs();	
+		MyNameIs();
+		MyNameIs();
+		
+		
+	}
+			
+		
+		static void MyNameIs() {
+			
+			System.out.println(" Meen Khatri");
+		}
+		
+	}
+
+
+	
