6370774f39404d8919d7f88772b5a8f662302908
ranagroup <erarana2020@gmail.com>
2019-06-19 16:08:46 -0400

    testing

--- Assignment9/MohammedChowdhury_assainmrnt9_q3.java
+++ Assignment9/MohammedChowdhury_assainmrnt9_q3.java
@@ -1,25 +1,28 @@
-package assainment9;
-
 public class MohammedChowdhury_assainmrnt9_q3 {
 	public static void main(String[]args) {
-		System.out.println(part1());
+		part1();
 		
 	}
-	public static int part1() {
-		   for(int i=0;i<15;i++) {
-		         for(int j=0;j<15-i;j++) {
-		        	 System.out.print(" ");
-		         }
-		        for(int k=0;k<=i;k++) {
-		            System.out.print(" *");
-		        }
-		        System.out.println();  
-		    }
-
+	public static void  part1() {
+		for(int i=0;i<=10;i++) {
+		    for(int j=1;j<=10-i;j++)
+		     System.out.print(" ");
+		     for(int j=1;j<=2*i-1;j++)
+		       System.out.print("*");
+		     System.out.print("\n");
+		}
+		 
+		   for(int i=10-1;i>=1;i--) {
+		   
+		     for(int j=1;j<=10-i;j++)
+		     System.out.print(" ");
+		     for(int j=1;j<=2*i-1;j++)
+		       System.out.print("*");
+		     System.out.print("\n");
+		   }
+		 
+		}
 		
-		return 0;
-		
-	}
-
 }
+
 
