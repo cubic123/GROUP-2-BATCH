2fc83de18520b28c226b3f68a9a2af5f28a7ed1b
ranagroup <erarana2020@gmail.com>
2019-06-20 16:37:40 -0400

    test

---
+++ Assignment10/MohammedChowdhury_assainment10/Professor.java
@@ -0,0 +1,24 @@
+package assainment10;
+
+
+
+public class Professor {
+
+		public  void Professorinfo() {
+			
+				int ID=1;
+				System.out.println("Professor ID "+ID);
+		String FirstName="kamal";
+		String LastName="Ahmed";
+		System.out.println("First Name is :"+FirstName);
+		System.out.println("Last Name is :"+LastName);
+		Department Departmentobject= new Department();
+		Departmentobject.Departmentinfo();
+
+		
+		
+
+		}
+
+	}
+	
---
+++ Assignment10/MohammedChowdhury_assainment10/School.java
@@ -0,0 +1,16 @@
+package assainment10;
+
+public class School {
+
+	public static void main(String[] args) {
+		
+		Student Studentobject= new Student();
+		Studentobject.Studentsinfo();
+		Professor Departmentobject= new Professor();
+		Departmentobject.Professorinfo();
+		
+	}
+
+}
+
+
