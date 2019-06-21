4a9806fcca45f764f6a3c3f8b1a4335f7c6527ca
raajmaharjan <raaj.maharjan@outlook.com>
2019-06-21 15:11:11 -0400

    Raj added the files to Assignment 8... finally!!!

---
+++ Assignment8/RajMaharjan_Assignment8_2.java
@@ -0,0 +1,17 @@
+public class RajMaharjan_Assignment8_2 {
+		
+	    public static void main(String[] args) {
+	       
+	    	int n = 0;
+	       
+	        for (int i = 0; i <= 100; i++) {
+	        
+	        n = n + i;  
+	        
+		System.out.println(" sum = "+  n);
+		
+
+	}
+	}
+
+}
---
+++ RajMaharjan_Assignment8_1.java
@@ -0,0 +1,11 @@
+public class RajMaharjan_Assignment8_1 {
+
+	public static void main(String[] args) {
+		
+		 int n = 1;
+				   
+		 while (n <= 20) {
+			 System.out.println("Raj Maharjan  " + n);
+			         n++;
+			      }
+			   }
